6df395c7b4c831f71fe0cd6a08a792ceb583d474
Brad Wallace <brad.wallace.13@gmail.com>
2014-02-25 00:28:39 -0500

    found a possible fix, need to make it dynamic now

--- responsive-attempt.js
+++ responsive-attempt.js
@@ -1,3 +1,12 @@
 (function($) {
-        	var wWidth = $(window).width();
+    $( window ).resize(function() {
+        var wWidth = $(window).width();
+
+        if(wWidth < 750){
+        	$("#item1").attr("data-col", "1");
+        	$("#item2").attr("data-col", "1");
+        	$("#item3").attr("data-col", "1");
+        	$("#item4").attr("data-col", "1");
+        }
+    })
 })(jQuery);
