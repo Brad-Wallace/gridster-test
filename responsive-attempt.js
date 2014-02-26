c9228b889db707e19f79531101b8cd34b6a4cd02
Brad Wallace <brad.wallace.13@gmail.com>
2014-02-25 22:51:09 -0500

    updated code to work for 2 different widths

--- responsive-attempt.js
+++ responsive-attempt.js
@@ -2,7 +2,7 @@
     $( window ).resize(function() {
         var wWidth = $(window).width();
 
-        if(wWidth < 750){
+        if(wWidth < 500){
 
         	$("#item1").attr("data-col", "1");
         	$("#item1").attr("data-row", "4");
@@ -32,7 +32,37 @@
         	$("#item9").attr("data-row", "3");
 
         }
-        if(wWidth > 750){        	
+        if(wWidth < 750 && wWidth > 500){
+
+        	$("#item1").attr("data-col", "2");
+        	$("#item1").attr("data-row", "2");
+
+        	$("#item2").attr("data-col", "2");
+        	$("#item2").attr("data-row", "3");
+
+        	$("#item3").attr("data-col", "1");
+        	$("#item3").attr("data-row", "4");
+
+        	$("#item4").attr("data-col", "1");
+        	$("#item4").attr("data-row", "5");
+
+        	$("#item5").attr("data-col", "2");
+        	$("#item5").attr("data-row", "1");
+
+        	$("#item6").attr("data-col", "3");
+        	$("#item6").attr("data-row", "1");
+
+        	$("#item7").attr("data-col", "4");
+        	$("#item7").attr("data-row", "1");
+
+        	$("#item8").attr("data-col", "4");
+        	$("#item8").attr("data-row", "2");
+
+        	$("#item9").attr("data-col", "4");
+        	$("#item9").attr("data-row", "3");
+
+        }
+        if(wWidth > 750){       	
 
         	$("#item1").attr("data-col", "2");
         	$("#item1").attr("data-row", "1");
