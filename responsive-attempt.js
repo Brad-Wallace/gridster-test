57672ede36cd9d3d82ddda605933a4bbb7f2ca2e
Brad Wallace <brad.wallace.13@gmail.com>
2014-02-25 22:59:03 -0500

    added another couple lines

--- responsive-attempt.js
+++ responsive-attempt.js
@@ -2,7 +2,37 @@
     $( window ).resize(function() {
         var wWidth = $(window).width();
 
-        if(wWidth < 500){
+        if(wWidth < 300){
+
+        	$("#item1").attr("data-col", "1");
+        	$("#item1").attr("data-row", "4");
+
+        	$("#item2").attr("data-col", "1");
+        	$("#item2").attr("data-row", "5");
+
+        	$("#item3").attr("data-col", "2");
+        	$("#item3").attr("data-row", "1");
+
+        	$("#item4").attr("data-col", "1");
+        	$("#item4").attr("data-row", "7");
+
+        	$("#item5").attr("data-col", "2");
+        	$("#item5").attr("data-row", "2");
+
+        	$("#item6").attr("data-col", "2");
+        	$("#item6").attr("data-row", "3");
+
+        	$("#item7").attr("data-col", "1");
+        	$("#item7").attr("data-row", "8");
+
+        	$("#item8").attr("data-col", "1");
+        	$("#item8").attr("data-row", "8");
+
+        	$("#item9").attr("data-col", "1");
+        	$("#item9").attr("data-row", "9");
+
+        }
+        if(wWidth > 300 && wWidth < 500){
 
         	$("#item1").attr("data-col", "1");
         	$("#item1").attr("data-row", "4");
@@ -32,7 +62,7 @@
         	$("#item9").attr("data-row", "3");
 
         }
-        if(wWidth < 750 && wWidth > 500){
+        if(wWidth > 500 && wWidth < 750){
 
         	$("#item1").attr("data-col", "2");
         	$("#item1").attr("data-row", "2");
